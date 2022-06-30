bacb1aea1a0d25b238e35c75be18fbb9ebb22ce9
binazgreat <binazgreat@gmail.com>
2022-06-30 12:49:58 -0700

    fixed command

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
-
-int main(void)
-{
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
-}
+/*
+ * main - Entry point
+ *
+ * Puts("\"Programming is like building a multilingual puzzle");
+ *
+ * Return: Always 0 (Success);
+ */
