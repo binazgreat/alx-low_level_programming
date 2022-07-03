91df2eac77c0ee1faf1c7d377c3246918a9feb38
binazgreat <binazgreat@gmail.com>
2022-07-03 09:15:57 -0700

    printting the exact ordered text

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,11 @@
-#include<stdio.h>
-/*
- * main - Entry point
+#include <stdio.h>
+/**
+ * main - print exactly Programming is like building a multilingual puzzle
  *
- * Puts("\"Programming is like building a multilingual puzzle");
- *
- * Return: Always 0 (Success);
+ * Return: 0 success
  */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
