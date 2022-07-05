b8762a84afc9c9368ea24843073c2322f63868d9
binazgreat <binazgreat@gmail.com>
2022-07-05 09:51:42 -0700

    checking lowercase character

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,6 @@
 #include "main.h"
 /**
- * prints_alphabet_10x - prints the lower case alphabets
- *
+ * void print_alphabet_10x - prints the lower case alphabets
  * Return: Always 0
  */
 void print_alphabet_x10(void)
