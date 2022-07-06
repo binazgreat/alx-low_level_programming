8d3d543ca1585a8c3f567d7badcc886942462c8b
binazgreat <binazgreat@gmail.com>
2022-07-06 00:17:01 -0700

    hot fix

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,12 +2,13 @@
 /**
  * jack_bauer - prints 24 hours
  */
-void jack_bauer(void){
+void jack_bauer(void)
+{
 	int i, j;
 
-	for (i = 0; i < 24; i++)
+	for (i = 0; i < 24; i ++)
 	{
-		for (j = 0; j < 60; j++)
+		for (j = 0; j < 60; j ++)
 		{
 			if (i < 10)
 			{
