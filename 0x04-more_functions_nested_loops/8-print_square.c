1d665f90a593b6994dd02fb544553dd2b85a5626
binazgreat <binazgreat@gmail.com>
2022-07-07 12:30:05 -0700

    hot fix

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_square - prints a square
+ * @size: integer parameters
+ */
+void print_square(int size)
+{
+	int x, y;
+	int size = 4
+
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		for (x = 0; x < size; x++)
+		{
+			for(y = 0; y < size; y++)
+			{
+				_putchar(35);
+			}
+			_putchar('\n');
+		}
+		_putchar('\n');
+	}
+}
