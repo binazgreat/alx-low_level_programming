e63bfcdb9c90bdb289b56fc6a119cdd17cf237ae
binazgreat <binazgreat@gmail.com>
2022-07-07 11:09:25 -0700

    hot fix

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,8 +12,8 @@ void more_numbers(void)
 		{
 			if (y >= 10)
 				_putchar((y / 10) + '0');
-				_putchar((y % 10) + '0');
+			_putchar((y % 10) + '0');
 		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
