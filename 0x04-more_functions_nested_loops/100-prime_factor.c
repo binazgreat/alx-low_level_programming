e0a72f15ef1d2d111d8d975af7386360c46ead10
binazgreat <binazgreat@gmail.com>
2022-07-08 11:58:10 -0700

    hot fix

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -2,13 +2,14 @@
 /**
  * main - Entry point
  *
- * Return: Always 0
+ * Return: Always 0.
  * */
 int main(void)
 {
 	long n, i;
+
 	n = 612852475143;
-	for (i =2; i < n; i++)
+	for (i = 2; i < n; i++)
 	{
 		while (n % i == 0)
 			n = n / i;
