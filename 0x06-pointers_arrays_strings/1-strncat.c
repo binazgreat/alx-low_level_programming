28d9931efb4c6a0652e369f584aed316641d62f4
binazgreat <binazgreat@gmail.com>
2022-07-14 13:03:57 -0700

    concatenate function created

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strncat - function that concatenates two strings
+ * @dest: First parameter
+ * @src: Second Parameter
+ * @n: Third parameter
+ * Return: a pointer to the resulting string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	strncat(dest, src, n);
+	return (dest);
+}
--- 0x06-pointers_arrays_strings/README.md
+++ 0x06-pointers_arrays_strings/README.md
@@ -1,2 +1,3 @@
 0x06-more pointers, arrays and strings
 0. strcat - Write a function that concatenates two strings.
+1. strncat - Write a function that concatenates two strings.
