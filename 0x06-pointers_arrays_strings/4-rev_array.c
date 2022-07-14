d3f079bcbb6e73a4f1015c3ae74d0a17ab2ed918
binazgreat <binazgreat@gmail.com>
2022-07-14 13:43:54 -0700

    hot fix

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -3,6 +3,7 @@
  * reverse_array - reversing an array of integers
  * @a: Array a
  * @n: Element of array
+ * Return: 0 (Success)
  */
 void reverse_array(int *a, int n)
 {
@@ -11,7 +12,7 @@ void reverse_array(int *a, int n)
 	p = a;
 	for (i = 0; i < n; i++)
 		p++;
-	for (k = 0; k < i / 2; k++)
+	for (k = 0; k < n / 2; k++)
 	{
 		aux = a[k];
 		a[k] = *p;
