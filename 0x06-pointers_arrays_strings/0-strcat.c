f2d568211fae2f33181a8eae2e718d581f5c21fc
binazgreat <binazgreat@gmail.com>
2022-07-14 11:11:27 +0300

    hot fix

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,13 +9,13 @@ char *_strcat(char *dest, char *src)
 {
 	int len = 0, i;
 
-	while (dest(dest[len])
-			len++;
-			for (i = 0; src[i] != 0; i++)
-			{
-			dest[len] = src[i];
-			len += 1;
-			}
-			dest[len] = '\0';
-			return (dest);
-			}
+	while (dest[len])
+		len++;
+	for (i = 0; src[i] != 0; i++)
+	{
+	dest[len] = src[i];
+	len += 1;
+	}
+	dest[len] = '\0';
+	return (dest);
+}
