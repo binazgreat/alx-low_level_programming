770a8475d3d9edf5792539dd0352ba1e154dbb49
binazgreat <binazgreat@gmail.com>
2022-07-15 14:14:25 -0700

    hot fix

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -44,4 +44,10 @@ char *add_strings(char *n1, char *n2, char *r, int r_index)
 		*(r + r_index) = (num % 10) + '0';
 		tens = num / 10;
 	}
+	for (; *n2; n2--; r_index--)
+	{
+		num = (*n2 - '0') + tens;
+		*(r + r_index) = (num % 10) + '0';
+		tens = num / 10;
+	}
 }
