15ad1d77e02c145c3382a257efb6cf076207463c
binazgreat <binazgreat@gmail.com>
2022-07-18 08:03:10 -0700

    hot fix

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -3,7 +3,7 @@
  * _strchr - locates the character string
  * @s: string given
  * @c: another char
- * Return: a pointer to the first occurence of a character
+ * Return: a string
  */
 char *_strchr(char *s, char c)
 {
