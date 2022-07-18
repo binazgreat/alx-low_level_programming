e166109b5b3d926096e603f76017972720dacd85
binazgreat <binazgreat@gmail.com>
2022-07-18 07:56:39 -0700

    character locating function created

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strchr - locates the character string
+ * @s: string given
+ * @c: another char
+ * Return: a pointer to the first occurence of a character
+ */
+char *_strchr(char *s, char c)
+{
+	int a;
+
+	while (1)
+	{
+		a = s++;
+		if (a == c)
+		{
+			return (s - 1);
+		}
+		if ( a == 0)
+		{
+			return (NULL);
+		}
+	}
+}
