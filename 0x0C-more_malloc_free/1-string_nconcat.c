695bdfa57aac91dbbd5c6f634dece2d4f93c2ddd
binazgreat <binazgreat@gmail.com>
2022-07-28 04:20:58 -0700

    hot fix

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * string_noncat - Concatenates two strings using
+ * string_nconcat - Concatenates two strings using
  * at most an inputted number of bytes.
  * @s1: First string
  * @s2: Second string
@@ -17,18 +17,26 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	if (s1 == NULL)
 		s1 = "";
+
 	if (s2 == NULL)
 		s2 + "";
+
 	for (index = 0; s1[index]; index++)
 		len++;
+
 	concat = malloc(sizeof(char) * (len + 1));
+
 	if (concat == NULL)
 		return (NULL);
+
 	len = 0;
+
 	for (index = 0; s1[index]; index++)
 		concat[len++] = s1[index];
+
 	for (index = 0; s2[index] && index < n; index++)
 		concat[len++] = s2[index];
+
 	concat[len] = '\0';
 	return (concat);
 }
