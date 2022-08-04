329ece8b5e33db071c85873ae9b947c31830bd52
binazgreat <binazgreat@gmail.com>
2022-08-04 10:52:51 -0700

    Hot Fix

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -10,7 +10,7 @@ void print_char(va_list arg);
 void print_int(va_list arg);
 void print_float(va_list arg);
 void print_string(va_list arg);
-void primt_all(const char * const  format, ...);
+void primt_all(const char * const format, ...);
 /**
  * print_char - Prints a char.
  * @arg: A list of arguments pointing to
