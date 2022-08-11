9859ba11a9598fecfba9bd80d999f39bda79a33e
Alexander-Sibhatu <sibhatualexander@gmail.com>
2022-08-11 10:14:25 -0700

    function that adds node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -15,7 +15,8 @@ list_t *add_node(list_t **head, const char *str)
 	if (new == NULL)
 		return (NULL);
 	new->str = strdup(str);
-	for (nchar = 0; str[nchar]; nchar++);
+	for (nchar = 0; str[nchar]; nchar++)
+		;
 
 	new->len = nchar;
 	new->next = *head;
